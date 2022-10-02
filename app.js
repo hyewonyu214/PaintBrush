99656867ddd3c107f28d7a318d95c694c2d93a8d
hyewonyu214 <gva05165@naver.com>
2022-10-02 22:30:59 +0900

    그림판 사진 삽입하기, 그림 저장하기

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const saveBtn = document.getElementById("save");
+const textInput = document.getElementById("text");
+const fileInput = document.getElementById("file");
 const modeBtn = document.getElementById("mode-btn");
 const destroyBtn = document.getElementById("destroy-btn");
 const eraserBtn = document.getElementById("eraser-btn");
@@ -15,6 +18,7 @@ const CANVAS_HEIGHT = 800;
 canvas.width = CANVAS_WIDTH;
 canvas.height = CANVAS_HEIGHT;
 ctx.lineWidth = lineWidth.value;
+ctx.lineCap ="round"
 let isPainting = false;
 let isFilling = false;
 
@@ -38,8 +42,8 @@ function onLineWidthChange(event){
 }
 
 function oncolorChange(event){
-    ctx.strokeStyle = event.target.value;
-    ctx.fillStyle= event.target.value;
+    ctx.strokeStyle = event.target.value; //라인 색깔 바꾸기
+    ctx.fillStyle= event.target.value;  //채우기 색깔 바꾸기
 }
 
 function onColorClick(event){
@@ -76,6 +80,37 @@ function onEraserClick(){
     modeBtn.innerText="Fill";
 }
 
+function onFileChange(event){
+    const file = event.target.files[0];
+    const url = URL.createObjectURL(file);
+    const image = new Image();
+    image.src=url;
+    image.onload=function(){
+        ctx.drawImage(image, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+        fileInput.value = null;
+    };
+}
+
+function onDoubleClick(event){
+    const text = textInput.value;
+    if(text !== "" ){
+    ctx.save(); //ctx의 현재 상태를 저장한다
+    ctx.lineWidth =1;
+    ctx.font="68px serif";
+    ctx.fillText(text, event.offsetX, event.offsetY); 
+    ctx.restore(); //기존의 체크포인트로 돌아감
+    }
+}
+
+function onSaveClick(){
+    const url = canvas.toDataURL();
+    const a = document.createElement("a")
+    a.href = url;
+    a.download="myDrawing.png";
+    a.click();
+}
+
+canvas.addEventListener("dblclick", onDoubleClick);
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
@@ -89,6 +124,8 @@ colorOptions.forEach(color =>color.addEventListener("click", onColorClick))
 modeBtn.addEventListener("click", onModeClick);
 destroyBtn.addEventListener("click",onDestroyClick);
 eraserBtn.addEventListener("click", onEraserClick);
+fileInput.addEventListener("change", onFileChange);
+saveBtn.addEventListener("click", onSaveClick);
 
 
 
